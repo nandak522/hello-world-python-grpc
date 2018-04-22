49fad5a51a366f24571ce39441c7086c8b0f0ead
Nanda Kishore <madhav.bnk@gmail.com>
2018-04-22 08:58:08 +0530

    Add correct reqs

--- setup.py
+++ setup.py
@@ -9,4 +9,10 @@ setup(name='hello_world_python_grpc',
       author_email='madhav.bnk@gmail.com',
       package_dir = {'': 'src'},
       packages=['hello_world_python_grpc', 'hello_world_python_grpc.service'],
+      install_requires=[
+        'grpcio == 1.11.0',
+        'grpcio-tools == 1.11.0',
+        'protobuf == 3.5.2.post1',
+        'six == 1.11.0',
+      ]
      )
