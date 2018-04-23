8f8664a644a229a1ca42a3b6158cf0067754cf2c
Nanda Kishore <madhav.bnk@gmail.com>
2018-04-23 12:37:46 +0530

    Reorganizing

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@
 
 from distutils.core import setup
 
-setup(name='hello_world_python_grpc',
-      version='1.0',
-      description='hello_world_python_grpc',
-      author='Nanda Kishore B',
-      author_email='madhav.bnk@gmail.com',
-      package_dir = {'': 'src'},
-      packages=['hello_world_python_grpc', 'hello_world_python_grpc.service'],
-      install_requires=[
+setup(
+    name='hello_world_python_grpc',
+    version='0.1.0',
+    description='hello_world_python_grpc',
+    author='Nanda Kishore B',
+    author_email='madhav.bnk@gmail.com',
+    package_dir={'': 'src'},
+    packages=['hello_world_python_grpc'],
+    install_requires=[
         'grpcio == 1.11.0',
         'grpcio-tools == 1.11.0',
         'protobuf == 3.5.2.post1',
         'six == 1.11.0',
-      ]
-     )
+    ])
